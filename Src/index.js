e1ddc2d202998092580133ef5935f4737ec75a18
unknown <vishnudinesh128@gmail.com>
2026-02-05 17:28:44 +0530

    Initial authentication server setup

---
+++ Src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Authentication server running ");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
