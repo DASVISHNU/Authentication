c532351d43d19a58e5b85f8de8a7af8f1756b9d5
unknown <vishnudinesh128@gmail.com>
2026-02-07 16:24:32 +0530

    added dotenv and used it

--- Src/index.js
+++ Src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv"
+dotenv.config({path:"./.env"})
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Authentication server running ");
