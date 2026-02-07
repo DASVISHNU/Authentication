711ce41e9c9369418a1b50017fea47671ff9b7d3
unknown <vishnudinesh128@gmail.com>
2026-02-07 17:55:40 +0530

    Made Database connection with mongodb atlas

--- Src/index.js
+++ Src/index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv"
 dotenv.config({path:"./.env"})
+import connectDB from "./db/index.js";
 
 const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Authentication server running ");
+  res.send("Authentication server running hehe ");
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+
+
+connectDB()
+.then(()=>{
+  app.listen(port,()=>{
+    console.log(`server running on port ${port}`);
+  });
+})
+.catch((err)=>{
+  console.log("mongodb connection error",err)
+  process.exit(1)
+})
