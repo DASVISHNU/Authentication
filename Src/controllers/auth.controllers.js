2e2b2aabd95c7e86973cc5d6f3e9a4179231c66d
unknown <vishnudinesh128@gmail.com>
2026-03-17 20:39:44 +0530

    added login user logic in authcontroller

--- Src/controllers/auth.controllers.js
+++ Src/controllers/auth.controllers.js
@@ -77,3 +77,50 @@ const registerUser=asyncHandler(async(req,res)=>{
     );
 
 });
+
+const login=asyncHandler(async (req,res)=>{
+    const{email,password,username}=req.body;
+
+    if(!email)
+    {
+        throw new ApiError(400,"email is required");
+
+    }
+    const user= await User.findOne({email});
+    if(!user)
+    {
+        throw new ApiError(400,"user does not exists");
+    }
+
+    const isPasswordValid= await user.isPasswordCorrect(password);
+    if(!isPasswordValid)
+    {
+        throw new ApiError(400,"Invalid credentials");
+    }
+
+    const {accessToken,refreshToken}= await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser= await User.findById(user._id).select(
+        "-password -refreshToken -emailVerficationToken -emailVerificationExpiry",
+    );
+
+    const options={
+        httpOnly:true,
+        secure:true,
+    };
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user:loggedInUser,
+                accessToken,
+                refreshToken,
+            },
+            "user logged in successfully",
+        ),
+    )
+})
